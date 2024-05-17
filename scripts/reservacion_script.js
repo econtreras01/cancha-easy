c597600f5c2dc6f615eded4af3a4c6da66821385
bvstiiaann <duoc.bastian@gmail.com>
2024-05-17 09:38:43 -0400

    sds

--- scripts/reservacion_script.js
+++ scripts/reservacion_script.js
@@ -1,18 +1,13 @@
-// Función para guardar la reserva en una lista
+
 function guardarReserva(deporte, dia, horas) {
-    // Obtener la lista de reservas del localStorage
     var reservas = JSON.parse(localStorage.getItem('reservas')) || [];
     
-    // Crear un objeto de reserva
     var reserva = {
         deporte: deporte,
         dia: dia,
         horas: horas
     };
-
-    // Agregar la reserva a la lista
     reservas.push(reserva);
 
-    // Guardar la lista actualizada en el localStorage
     localStorage.setItem('reservas', JSON.stringify(reservas));
 }
