2ddf4dc815adc54cffe472869a7a0e11f0e10620
Eliu Contreras <eliucontrerasmj2@gmail.com>
2024-06-28 02:09:17 -0400

    Evaluación

--- scripts/calendario_script.js
+++ scripts/calendario_script.js
@@ -1,7 +1,7 @@
 let monthName = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
     "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-
-let selectedDates = []; // Array para almacenar las fechas seleccionadas
+let selectedHours = [];
+let selectedDay = null;
 
 let now = new Date();
 let day = now.getDate();
@@ -10,118 +10,203 @@ let currentMonth = month;
 let year = now.getFullYear();
 
 $(document).ready(function () {
-    initCalendar(); // Llamada a la función para inicializar el calendario
+    
+    initCalendar(); 
 
-    // Establecer el evento de clic para el botón de siguiente mes
     $("#next_month").click(function () {
         getNextMonth();
     });
 
-    // Establecer el evento de clic para el botón de mes anterior
     $("#last_month").click(function () {
         getPrevMonth();
     });
-});
 
-function initCalendar() {
-    // Establecer el nombre del mes y el año en el calendario
-    $("#text_month_02").text(monthName[month]);
-    $("#text_year").text(year);
+    function initCalendar() {
+        $(".item_day").remove();
 
-    // Eliminar los días previamente generados
-    $(".item_day").remove();
+        $("#text_month_02").text(monthName[month]);
+        $("#text_year").text(year);
 
-    // Generar los días del mes anterior (si es necesario)
-    for (let i = startDay(); i > 0; i--) {
-        $(".container_days").append(`<span class="week_days_item item_day prev_days">${getTotalDays(month - 1) - (i - 1)}</span>`);
-    }
-
-    // Generar los días del mes actual
-    for (let i = 1; i <= getTotalDays(month); i++) {
-        let $day = $(`<span class="week_days_item item_day calendar-day">${i}</span>`);
-        let currentDate = new Date(year, month, i);
-        if (currentDate < now && (currentDate.getDate() !== day || currentDate.getMonth() !== currentMonth || currentDate.getFullYear() !== now.getFullYear())) {
-            $day.addClass("disabled"); // Deshabilitar días anteriores al día actual, excepto el día actual
-            $day.addClass("todaynot"); // Resaltar los días deshabilitados
-        } else if (currentDate.getDate() === day && currentDate.getMonth() === currentMonth) {
-            // Resaltar el día actual
-            if (now.getHours() >= 19) {
-                $day.addClass("todaynot"); // Marcar como todaynot si son pasadas las 19:00
-                $day.addClass("disabled");
-                $day.addClass("today");
-            } else {
-                $day.addClass("today");
-            }
+        for (let i = startDay(); i > 0; i--) {
+            $(".container_days").append(`<span class="week_days_item item_day prev_days">${getTotalDays(month - 1) - (i - 1)}</span>`);
         }
-        $(".container_days").append($day);
+
+        for (let i = 1; i <= getTotalDays(month); i++) {
+            let $day = $(`<span class="week_days_item item_day calendar-day">${i}</span>`);
+            let currentDate = new Date(year, month, i);
+
+            if (currentDate < now && (currentDate.getDate() !== day || currentDate.getMonth() !== currentMonth || currentDate.getFullYear() !== now.getFullYear())) {
+                $day.addClass("disabled");
+                $day.addClass("todaynot");
+            } else if (currentDate.getDate() === day && currentDate.getMonth() === currentMonth) {
+                if (now.getHours() >= 19) {
+                    $day.addClass("todaynot");
+                    $day.addClass("disabled");
+                    $day.addClass("today");
+                } else {
+                    $day.addClass("today");
+                }
+            }
+
+            $day.click(function () {
+                if ($(this).hasClass("disabled")) return;
+                $(".calendar-day").removeClass("selected");
+                $(this).addClass("selected");
+                
+                selectedDay = new Date(year, month, parseInt($(this).text()));
+                clearSelectedHours();
+                updateReservaMensaje();
+            });
+
+            $(".container_days").append($day);
+        }
+
+        $(".hours_container").empty();
+        for (let i = 10; i <= 19; i++) {
+            $(".hours_container").append(`<div class="hour_item" id="hour_${i}" data-hour="${i}">${i}:00</div>`);
+        }
+
+        $(".hour_item").click(function () {
+            if ($(this).hasClass("disabled") || !selectedDay || isPastHour($(this).data("hour"))) return;
+            $(this).toggleClass("selected");
+            let hour = $(this).data("hour");
+            let index = selectedHours.indexOf(hour);
+            if (index === -1) {
+                selectedHours.push(hour);
+            } else {
+                selectedHours.splice(index, 1);
+            }
+
+            let newDate = new Date(selectedDay.getTime());
+            newDate.setHours(hour);
+            selectedDay = newDate;
+
+            updateReservaMensaje();
+        });
     }
 
+    function getNextMonth() {
+        if (month !== 11) {
+            month++;
+        } else {
+            year++;
+            month = 0;
+        }
+        initCalendar();
+    }
 
-    // Establecer el evento de clic para cada día del calendario
-    $(".calendar-day").click(function () {
-        if ($(this).hasClass("disabled")) return; // Salir si el día está deshabilitado
+    function getPrevMonth() {
+        if (month !== 0) {
+            month--;
+        } else {
+            year--;
+            month = 11;
+        }
+        initCalendar();
+    }
 
-        // Remover la clase "selected" de todos los días y seleccionar solo el día clickeado
-        $(".calendar-day").removeClass("selected");
-        $(this).addClass("selected");
+    function startDay() {
+        let start = new Date(year, month, 1);
+        return ((start.getDate() - 1) === -1) ? 6 : start.getDay();
+    }
 
-        let date = new Date(year, month, parseInt($(this).text())); // Crear objeto de fecha
-        let dateString = date.toISOString().slice(0, 10); // Convertir la fecha a una cadena ISO
+    function leapMonth() {
+        return ((year % 400 === 0) || (year % 4 === 0) && (year % 100 !== 0));
+    }
 
-        selectedDates = []; // Reiniciar el array de fechas seleccionadas
-        selectedDates.push(dateString); // Agregar la fecha seleccionada al array
+    function getTotalDays() {
+        if (month === -1) month = 11;
 
-        console.log("Selected date:", dateString); // Mostrar la fecha seleccionada en la consola
+        let numMonthReal = month + 1;
+
+        if (numMonthReal == 3 || numMonthReal == 5 || numMonthReal == 8 || numMonthReal == 10) {
+            return 31;
+        } else if (numMonthReal == 0 || numMonthReal == 2 || numMonthReal == 4 || numMonthReal == 6
+            || numMonthReal == 7 || numMonthReal == 9 || numMonthReal == 10) {
+            return 30;
+        } else {
+            return leapMonth() ? 29 : 28;
+        }
+    }
+
+    $("#guardarReserva").click(function () {
+        if (!selectedDay || selectedHours.length === 0) return;
+        
+        let usuarioId = '12345678-9';
+        let canchaId = 1;
+        let año = selectedDay.getFullYear();
+        let mes = ('0' + (selectedDay.getMonth() + 1)).slice(-2);
+        let dia = ('0' + selectedDay.getDate()).slice(-2);
+        let fechaReserva = `${año}-${mes}-${dia}`;
+        
+        let horaInicio = selectedDay.toLocaleTimeString('es-CL', { hour12: false, hour: '2-digit', minute: '2-digit' });
+        selectedDay.setHours(selectedDay.getHours() + 1);
+        let horaFin = selectedDay.toLocaleTimeString('es-CL', { hour12: false, hour: '2-digit', minute: '2-digit' });
+        
+        let estado = 1;
+
+        let reservaData = {
+            usuario: usuarioId,
+            cancha: canchaId,
+            fecha: fechaReserva,
+            hora_inicio: horaInicio,
+            hora_fin: horaFin,
+            estado: estado
+        };
+
+        console.log(reservaData);
+
+        $.ajax({
+            url: 'http://127.0.0.1:8000/api/reservas/',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(reservaData),
+            success: function (response) {
+                console.log('Reserva creada:', response);
+                alert('Reserva creada exitosamente con ID: ' + response.id_reserva);
+                window.location.href = "reservalista.html?id=" + response.id_reserva;
+            },
+            error: function (error) {
+                console.error('Error al crear reserva:', error);
+                alert('Error al crear reserva.');
+            }
+        });
     });
-}
 
-// Función para avanzar al siguiente mes
-function getNextMonth() {
-    if (month !== 11) {
-        month++;
-    } else {
-        year++;
-        month = 0;
+    function isPastHour(hour) {
+        let now = new Date();
+        let currentHour = now.getHours();
+        let selectedDate = new Date(selectedDay);
+        if (now.getDate() === selectedDate.getDate() && now.getMonth() === selectedDate.getMonth() && now.getFullYear() === selectedDate.getFullYear()) {
+            return hour <= currentHour;
+        }
+        return false;
     }
-    initCalendar(); // Volver a generar el calendario para el nuevo mes
-}
 
-// Función para retroceder al mes anterior
-function getPrevMonth() {
-    if (month !== 0) {
-        month--;
-    } else {
-        year--;
-        month = 11;
+    function updateReservaMensaje() {
+        let mensaje = "RESERVA ";
+        if (selectedDay) {
+            let fecha = new Date(selectedDay);
+            let mes = monthName[fecha.getMonth()];
+            let año = fecha.getFullYear();
+            let dia = fecha.getDate();
+            mensaje += `${mes} ${año} día ${dia} : `;
+        }
+        if (selectedHours.length > 0) {
+            selectedHours.sort(function (a, b) {
+                return a - b;
+            });
+            mensaje += selectedHours.map(hour => `${hour}:00hr`).join(" - ");
+        }
+        $("#reserva_mensaje").text(mensaje);
+
+        $("#guardarReserva").prop('disabled', !selectedDay || selectedHours.length === 0);
     }
-    initCalendar(); // Volver a generar el calendario para el nuevo mes
-}
 
-// Función para obtener el día de la semana en que comienza el mes
-function startDay() {
-    let start = new Date(year, month, 1);
-    return ((start.getDate() - 1) === -1) ? 6 : start.getDay();
-}
-
-// Función para determinar si el año actual es bisiesto
-function leapMonth() {
-    return ((year % 400 === 0) || (year % 4 === 0) && (year % 100 !== 0));
-}
-
-// Función para obtener el número total de días en el mes actual
-function getTotalDays() {
-    if (month === -1) month = 11;
-
-    let numMonthReal = month + 1;
-
-    if (numMonthReal == 3 || numMonthReal == 5 || numMonthReal == 8 || numMonthReal == 10) {
-        return 31;
-    } else if (numMonthReal == 0 || numMonthReal == 2 || numMonthReal == 4 || numMonthReal == 6
-        || numMonthReal == 7 || numMonthReal == 9 || numMonthReal == 10) {
-        return 30;
-    } else {
-        return leapMonth() ? 29 : 28;
+    function clearSelectedHours() {
+        $(".hour_item").removeClass("selected");
+        selectedHours = [];
+        updateReservaMensaje();
     }
-}
 
-
+});
